aed4fb2bca1fafacf6c91be59a33120a85953c74
alancrps <110501681+alancrps@users.noreply.github.com>
2023-08-08 19:27:31 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("HOLA DESDE LA API");
+});
+
+app.listen(1000, () => {
+	console.log(`⚡️ SERVER API IS RUNNING http://localhost:1000`);
+});
+
+app.get("/usuarios/:usuarioId", (req: Request, res: Request) => {
+	const usuarios: Array<any> = [
+		{
+			id: 1,
+			email: "usuario1@example.com",
+			nombre: "Juan",
+			apellido: "Pérez",
+			edad: 28,
+			activo: true,
+		},
+		{
+			id: 2,
+			email: "usuario2@example.com",
+			nombre: "María",
+			apellido: "González",
+			edad: 35,
+			activo: false,
+		},
+	];
+
+	// const u = usuarios.filter((u) => {
+	// 	return u.id === req.params.usuarioId;
+	// });
+
+	const u = usuarios.map((u) => {
+		if (u.id == req.params.usuarioId) {
+			return u;
+		}
+	});
+
+	if (u.length <= 0) {
+		res.status(404).json({
+			msg: `No se encontro ningun usuario con el id: ${req.params.usuarioId}`,
+		});
+	}
+
+	res.json(u);
+});
